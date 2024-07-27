7ae8e6ae32cf7f13687632a4613e5c9f0638ae5d
Beknur Patsaev <patsaev1994@mail.ru>
2024-07-27 18:07:07 +0300

    Initial Commit

---
+++ SwiftUIStudy/SwiftUIStudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIStudyApp.swift
+//  SwiftUIStudy
+//
+//  Created by Beknur Patsaev on 27.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIStudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
